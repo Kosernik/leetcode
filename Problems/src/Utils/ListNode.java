48e87f0fecdf62d3f7d119f7ac9acd4dfe1c69d6
Kosernik <kornilovsergei28@gmail.com>
2020-07-10 20:02:00 +0300

    Добавлено два конструктора во вспомогательный класс ListNode.

--- Problems/src/Utils/ListNode.java
+++ Problems/src/Utils/ListNode.java
@@ -3,10 +3,12 @@ package Utils;
 public class ListNode {
     public int val;
     public ListNode next;
+    public ListNode() {}
     public ListNode(int x) {
         val = x;
         next = null;
     }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
     public int getVal() {
         return val;
