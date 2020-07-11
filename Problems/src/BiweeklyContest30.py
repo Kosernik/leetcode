c81f6edaccc805ff5fbb4ac79b84c52244e5cf4e
Sergei <47776753+Kosernik@users.noreply.github.com>
2020-07-11 19:52:17 +0300

    Update BiweeklyContest30.py
    Исправлена опечатка.

--- Problems/src/BiweeklyContest30.py
+++ Problems/src/BiweeklyContest30.py
@@ -7,7 +7,7 @@ class Solution:
         dates = date.split(" ")
         months = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06",
                   "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
-        day = str(dates[0][:2]) if dates[0][1].isdigit() else ("0" + str(dates[0][1]))
+        day = str(dates[0][:2]) if dates[0][1].isdigit() else ("0" + str(dates[0][0]))
         result = dates[2] + "-" + months.get(dates[1]) + "-" + day
         return result
 
