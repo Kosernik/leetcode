4b43be940e8c4591120b6f6e513c9267b41e85eb
Kosernik <kornilovsergei28@gmail.com>
2021-01-15 15:54:06 +0300

    LeetCode #1646.

---
+++ Problems/src/MonthlyChallenges/January21/GetMaximumInGeneratedArray.java
@@ -0,0 +1,28 @@
+package MonthlyChallenges.January21;
+
+import java.util.Arrays;
+
+public class GetMaximumInGeneratedArray {
+    public static void main(String[] args) {
+        GetMaximumInGeneratedArray solution = new GetMaximumInGeneratedArray();
+        System.out.println(solution.getMaximumGenerated(7));
+    }
+
+    // LeetCode #1646.
+    public int getMaximumGenerated(int n) {
+        if (n == 0) return 0;
+
+        int max = 1;
+        int[] array = new int[n+1];
+        array[1] = 1;
+
+        for (int i = 1; (2 * i + 1) <= n; i++) {
+            array[2*i] = array[i];
+            array[2*i + 1] = array[i] + array[i+1];
+            max = Math.max(max, array[2*i]);
+            max = Math.max(max, array[2*i+1]);
+        }
+
+        return max;
+    }
+}
