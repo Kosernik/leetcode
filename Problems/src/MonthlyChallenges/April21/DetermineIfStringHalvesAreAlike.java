b23de1a04ab18477bd954fffd3e68939a50ffd68
Kosernik <kornilovsergei28@gmail.com>
2021-04-07 10:20:54 +0300

    LeetCode #1704.

---
+++ Problems/src/MonthlyChallenges/April21/DetermineIfStringHalvesAreAlike.java
@@ -0,0 +1,46 @@
+package MonthlyChallenges.April21;
+
+public class DetermineIfStringHalvesAreAlike {
+    /**
+     * LeetCode #1704.
+     *
+     * Checks if two halves of a string have the same number of vowels.
+     *
+     * Complexity - O(N)
+     * Memory - O(1)
+     *
+     * @param s - a string of english letters, s.length() is even.
+     * @return - true - if both halves of a string have the same number of vowels, false - otherwise.
+     */
+    public boolean halvesAreAlike(String s) {
+        int vowelsCount = 0;
+
+        for (int i = 0; i < s.length()/2; i++) {
+            if (isVowel(s.charAt(i))) {
+                vowelsCount++;
+            }
+        }
+        for (int i = s.length()/2; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                vowelsCount--;
+            }
+        }
+
+        return vowelsCount == 0;
+    }
+
+    private boolean isVowel(char ch) {
+        if ('a' <= ch && ch <= 'z') {
+            return isLowerVowel(ch);
+        } else {
+            return isCapitalVowel(ch);
+        }
+    }
+
+    private boolean isLowerVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    private boolean isCapitalVowel(char ch) {
+        return ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+}
