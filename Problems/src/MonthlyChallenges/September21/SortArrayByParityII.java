2c1b624ed8f6d9ede71dbb0059686639db2573f1
Kosernik <kornilovsergei28@gmail.com>
2021-09-28 15:50:55 +0300

    LeetCode #922.

---
+++ Problems/src/MonthlyChallenges/September21/SortArrayByParityII.java
@@ -0,0 +1,32 @@
+package MonthlyChallenges.September21;
+
+import java.util.ArrayDeque;
+
+public class SortArrayByParityII {
+    /**
+     * LeetCode #922. Sort Array By Parity II.
+     *
+     * Complexity - O(N)
+     * Memory - O(N)
+     *
+     * @param nums - an array of integers. nums.length is even. Half of the integers in nums are even.
+     * @return - an array sorted by parity.
+     */
+    public int[] sortArrayByParityII(int[] nums) {
+        int[] result = new int[nums.length];
+        int idxEven = 0;
+        int idxOdd = 1;
+
+        for (int number : nums) {
+            if (number % 2 == 0) {
+                result[idxEven] = number;
+                idxEven += 2;
+            } else {
+                result[idxOdd] = number;
+                idxOdd += 2;
+            }
+        }
+
+        return result;
+    }
+}
