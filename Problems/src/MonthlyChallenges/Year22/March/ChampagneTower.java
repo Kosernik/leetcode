c446bd1fff26a93b6ebd3a368c3420fe31b20ac0
Kosernik <kornilovsergei28@gmail.com>
2022-03-04 14:28:27 +0300

    LeetCode #799.

---
+++ Problems/src/MonthlyChallenges/Year22/March/ChampagneTower.java
@@ -0,0 +1,34 @@
+package MonthlyChallenges.Year22.March;
+
+public class ChampagneTower {
+
+    /**
+     * LeetCode #799. Champagne Tower.
+     *
+     * Complexity - O(N*M), N = query_row, M = query_glass.
+     * Memory - O(N)
+     *
+     * @param poured - the number of cups of champagne.
+     * @param query_row - the index of a row of the pyramid.
+     * @param query_glass - the index of a glass in the target row.
+     * @return - the amount of champagne in the target glass.
+     */
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[] row = new double[1];
+        row[0] = poured;
+
+        for (int i = 1; i <= query_row; i++) {
+            double[] curRow = new double[row.length+1];
+
+            for (int col = 0; col < row.length; col++) {
+                double half = Math.max((row[col] - 1)  / 2, 0);
+                curRow[col] += half;
+                curRow[col+1] += half;
+            }
+
+            row = curRow;
+        }
+
+        return Math.min(row[query_glass], 1);
+    }
+}
