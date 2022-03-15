3445f341490779fbfd561c9aa3085ffa4a6e8299
Kosernik <kornilovsergei28@gmail.com>
2022-03-15 18:52:45 +0300

    Added non-recursive method for SberCraft task #3. Airplane.

--- Problems/src/SberCraft/Airplane.java
+++ Problems/src/SberCraft/Airplane.java
@@ -1,6 +1,20 @@
 package SberCraft;
 
 public class Airplane {
+    public static void main(String[] args) {
+        int numberOfErrors = 0;
+        for (int x = 1; x < 100; x++) {
+            for (int y = 1; y < 100; y++) {
+                int first = brokenDevice(x, y);
+                int second = brokenDeviceAlt(x, y);
+                if (first != second) {
+                    System.out.println("X: " + x + " y: " + y + "\t" + first + "\t" + second + "\t");
+                    numberOfErrors++;
+                }
+            }
+        }
+        System.out.println("Number of errors: " + numberOfErrors);
+    }
 
     public static int brokenDevice(int x, int y) {
         //int ans = 0;
@@ -21,5 +35,20 @@ public class Airplane {
         } else {
             return brokenDevice(x, y + 1) + 1;
         }
+    }
+
+    public static  int brokenDeviceAlt(int x, int y) {
+        int ans = 0;
+
+        while (y > x) {
+            if (y % 2 == 0) {
+                y /= 2;
+            } else {
+                y += 1;
+            }
+            ans++;
+        }
+
+        return ans + (x-y);
     }
 }
