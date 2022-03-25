a3b4a789041751d99da25a575fd1807ac0e50546
Kosernik <kornilovsergei28@gmail.com>
2022-03-25 12:02:36 +0300

    Added better solution for LeetCode #556.

--- Problems/src/MonthlyChallenges/December/NextGreaterElementIII.java
+++ Problems/src/MonthlyChallenges/December/NextGreaterElementIII.java
@@ -1,16 +1,93 @@
 package MonthlyChallenges.December;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class NextGreaterElementIII {
+    public static void main(String[] args) {
+        NextGreaterElementIII solution = new NextGreaterElementIII();
+
+        int[] tests = {12, 21, 123, 2, 9, 2147483647, 43210, 2147483646, 2122222222, 230241};
+        int[] results = {21, -1, 132, -1, -1, -1, -1, -1, -1, 230412};
+
+        for (int i = 0; i < tests.length; i++) {
+            int res = solution.nextGreaterElement(tests[i]);
+            if (res != results[i]) {
+                System.out.println("Got " + res + ", instead of " + results[i] + " for number " + tests[i]);
+            }
+        }
+
+        Random random = new Random();
+        System.out.println("Randoms:");
+
+        for (int i = 0; i < 123; i++) {
+            int n = random.nextInt(Integer.MAX_VALUE);
+            int res = solution.nextGreaterElement(n);
+            int resB = solution.nextGreaterElementBrute(n);
+            if (res != resB) {
+                System.out.println("Got " + res + ", instead of " + resB + " for number " + n);
+            }
+        }
+
+        System.out.println("Done!");
+    }
+
+    /**
+     * LeetCode #556.
+     *
+     *
+     * Generates the smallest integer which has exactly the same digits existing in the integer "n" and is greater in
+     * value than "n".
+     *
+     * @param n - positive integer.
+     * @return - the smallest integer which has exactly the same digits existing in the integer "n" and is greater in
+     * value than "n".
+     */
+    public int nextGreaterElement(int n) {
+        if (n <= 9) return -1;
+
+        char[] number = String.valueOf(n).toCharArray();
+
+        for (int i = number.length - 2; i >= 0; i--) {
+            if (number[i] < number[i+1]) {
+                int idx = i+1;
+                int candidate = number[i+1];
+                int candidateIdx = i+1;
+                while (idx < number.length) {
+                    if (number[idx] > number[i] && number[idx] < candidate) {
+                        candidate = number[idx];
+                        candidateIdx = idx;
+                    }
+                    idx++;
+                }
+                swap(number, i, candidateIdx);
+
+                char[] rightPart = Arrays.copyOfRange(number, i+1, number.length);
+                Arrays.sort(rightPart);
+                for (int j = 0; j < rightPart.length; j++) {
+                    number[i+j+1] = rightPart[j];
+                }
+
+                break;
+            }
+        }
+
+        long result = Long.parseLong(new String(number));
+
+        if (result == n || result > Integer.MAX_VALUE) return -1;
+        else return (int) result;
+    }
+
+    private void swap(char[] number, int a, int b) {
+        char temp = number[a];
+        number[a] = number[b];
+        number[b] = temp;
+    }
+
 
     /**
      * LeetCode #556.
      * (BruteForce solution)
-     *
+     * <p>
      * Generates the smallest integer which has exactly the same digits existing in the integer "n" and is greater in
      * value than "n".
      *
---
+++ Problems/src/Problems/NextGreaterElementII.java
@@ -0,0 +1,13 @@
+package Problems;
+
+public class NextGreaterElementII {
+
+    public int[] nextGreaterElements(int[] nums) {
+        int length = nums.length;;
+        int[] result = new int[length];
+
+
+
+        return result;
+    }
+}
