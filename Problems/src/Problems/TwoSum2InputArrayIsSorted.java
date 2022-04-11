c2fa7f6a7c1e422a9a69cee22b3b3ac811105d02
Kosernik <kornilovsergei28@gmail.com>
2022-04-11 13:39:26 +0300

    LeetCode #167. Fixed memory complexity for 2-pointers algorithm. Added a binary search algorithm for the problem.

--- Problems/src/Problems/TwoSum2InputArrayIsSorted.java
+++ Problems/src/Problems/TwoSum2InputArrayIsSorted.java
@@ -6,7 +6,7 @@ public class TwoSum2InputArrayIsSorted {
      * LeetCode #167. Two Sum II - Input Array Is Sorted.
      *
      * Complexity - O(N)
-     * Memory O(N)
+     * Memory O(1)
      *
      * @param numbers - a 1-indexed array of integers that is already sorted in non-decreasing order.
      * @param target - required sum.
@@ -22,5 +22,56 @@ public class TwoSum2InputArrayIsSorted {
             else if (curSum < target) left++;
             else right--;
         }
+    }
+
+
+    /**
+     * LeetCode #167. Two Sum II - Input Array Is Sorted.
+     *
+     * Complexity - O(NlogN)
+     * Memory O(1)
+     *
+     * @param numbers - a 1-indexed array of integers that is already sorted in non-decreasing order.
+     * @param target - required sum.
+     * @return - an array of indices of two numbers such that they add up to a "target" number.
+     *           1 <= index1 < index2 <= numbers.length. "numbers" contains exactly one solution.
+     */
+    public int[] twoSumBinarySearch(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            int remainder = target - numbers[i];
+
+            if (numbers[i] == remainder) {
+                if (numbers[i+1] == remainder) {
+                    return new int[] {i+1, i+2};
+                }
+                continue;
+            }
+
+            int idx = binarySearch(numbers, remainder);
+
+            if (idx != -1) {
+                return new int[] {i+1, idx+1};
+            }
+        }
+
+        return null;
+    }
+
+    private int binarySearch(int[] numbers , int target) {
+        int left = 0, right = numbers.length-1, middle;
+
+        while (left <= right) {
+            middle = (right - left) / 2 + left;
+
+            if (numbers[middle] == target) {
+                return middle;
+            } else if (numbers[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
     }
 }
