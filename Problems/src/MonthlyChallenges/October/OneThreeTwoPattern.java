d07d23a4220c4cb71eae8732611e8cd05f713b78
Kosernik <kornilovsergei28@gmail.com>
2022-05-08 00:58:34 +0300

    LeetCode #456. Optimized solution.

--- Problems/src/MonthlyChallenges/October/OneThreeTwoPattern.java
+++ Problems/src/MonthlyChallenges/October/OneThreeTwoPattern.java
@@ -3,6 +3,8 @@ package MonthlyChallenges.October;
 public class OneThreeTwoPattern {
 
     /**
+     * LeetCode #456. 132 Pattern.
+     *
      * Returns if there is a 132-pattern in a given array.
      *
      * Complexity - O(N^2)
@@ -22,6 +24,42 @@ public class OneThreeTwoPattern {
                 if (nums[j] < nums[i] && minValue < nums[j]) return true;
             }
         }
+        return false;
+    }
+
+
+    /**
+     * LeetCode #456. 132 Pattern.
+     *
+     * Returns if there is a 132-pattern in a given array.
+     *
+     * Complexity - O(N)
+     * Memory - O(1)
+     *
+     * @param nums - array of integers.
+     * @return - True - if there is a 132-pattern in an array, false - otherwise.
+     */
+    public boolean find132patternAlt(int[] nums) {
+        if (nums == null || nums.length <= 2) return false;
+
+        int length = nums.length;
+        int middleNumber = Integer.MIN_VALUE;
+        int stackIdx = length;
+
+        for (int i = length-1; i >= 0; i--) {
+            int curNumber = nums[i];
+
+            if (curNumber < middleNumber) return true;
+
+            while (stackIdx < length && curNumber > nums[stackIdx]) {
+                middleNumber = nums[stackIdx];
+                stackIdx++;
+            }
+
+            stackIdx--;
+            nums[stackIdx] = curNumber;
+        }
+
         return false;
     }
 }
