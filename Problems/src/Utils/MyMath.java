6fcb1893eccbac8bb597772c7774a4588a99988a
Kosernik <kornilovsergei28@gmail.com>
2022-06-28 17:50:42 +0300

    Added a method to compute the Least Common Multiple.

--- Problems/src/Utils/MyMath.java
+++ Problems/src/Utils/MyMath.java
@@ -1,8 +1,29 @@
 package Utils;
 
 public class MyMath {
+    /**
+     * Greatest Common Divider
+     *
+     * @param a - a positive integer.
+     * @param b - a positive integer.
+     * @return - the greatest common divider.
+     */
     public static int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
+    }
+
+
+    /**
+     * Least Common Multiple
+     *
+     * @param a - a positive integer.
+     * @param b - a positive integer.
+     * @return - the least common multiple.
+     */
+    public static int lcm(int a, int b) {
+        long product = (long) a * b;
+
+        return (int) (product / gcd(a, b));
     }
 }
