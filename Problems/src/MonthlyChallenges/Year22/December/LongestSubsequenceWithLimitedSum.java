22e4e44f7324d5a0849d6a758f28d4b068f7de35
kosernik <kornilovsergei28@gmail.com>
2022-12-25 23:30:57 +0300

    LeetCode #2389. Code reformatted.

--- Problems/src/MonthlyChallenges/Year22/December/LongestSubsequenceWithLimitedSum.java
+++ Problems/src/MonthlyChallenges/Year22/December/LongestSubsequenceWithLimitedSum.java
@@ -1,19 +1,19 @@
 package MonthlyChallenges.Year22.December;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class LongestSubsequenceWithLimitedSum {
     public static void main(String[] args) {
         LongestSubsequenceWithLimitedSum solution = new LongestSubsequenceWithLimitedSum();
 
         System.out.println("Test 0");
-        int[] nums0 = {4,5,2,1};
-        int[] queries0 = {3,10,21};
+        int[] nums0 = {4, 5, 2, 1};
+        int[] queries0 = {3, 10, 21};
         System.out.println(Arrays.toString(solution.answerQueries(nums0, queries0)));
         System.out.println();
 
         System.out.println("Test 1");
-        int[] nums1 = {2,3,4,5};
+        int[] nums1 = {2, 3, 4, 5};
         int[] queries1 = {1};
         System.out.println(Arrays.toString(solution.answerQueries(nums1, queries1)));
         System.out.println();
@@ -22,7 +22,7 @@ public class LongestSubsequenceWithLimitedSum {
     /**
      * LeetCode #2389. Longest Subsequence With Limited Sum.
      *
-     * @param nums - an array of positive integers.
+     * @param nums    - an array of positive integers.
      * @param queries - an array of positive integers.
      * @return - an array of lengths of the longest subsequences
      */
@@ -33,7 +33,7 @@ public class LongestSubsequenceWithLimitedSum {
         int[] prefixSum = new int[nums.length];
         prefixSum[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            prefixSum[i] = prefixSum[i-1] + nums[i];
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
         for (int i = 0; i < queries.length; i++) {
