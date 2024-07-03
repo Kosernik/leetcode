86bfa4e00f73f8c42aa4008611896e6dc7e9567d
Kosernik <kornilovsergei28@gmail.com>
2024-07-03 11:10:59 +0300

    LeetCode №1509. Added a faster realisation.

--- Problems/src/MonthlyChallenges/Year24/July/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ Problems/src/MonthlyChallenges/Year24/July/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -1,8 +1,16 @@
 package MonthlyChallenges.Year24.July;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
+    public static void main(String[] args) {
+        MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves solution = new MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves();
+
+        int[] test1 = {1, 5, 0, 10, 14};
+        System.out.println(solution.minDifference(test1) == 1);
+    }
 
     /**
      * LeetCode №1509. Minimum Difference Between Largest and Smallest Value in Three Moves.
@@ -14,7 +22,7 @@ public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
      * @return - the minimum difference between the largest and smallest value of nums after performing at most three
      * moves. In one move, you can choose one element of nums and change it to any value.
      */
-    public int minDifference(int[] nums) {
+    public int minDifferenceAlt(int[] nums) {
         Arrays.sort(nums);
         int minDifference = nums[nums.length - 1] - nums[0];
         if (nums.length <= 3) return 0;
@@ -25,5 +33,43 @@ public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
         }
 
         return minDifference;
+    }
+
+    /**
+     * LeetCode №1509. Minimum Difference Between Largest and Smallest Value in Three Moves.
+     * <p>
+     * Complexity - O(N)
+     * Memory - O(1)
+     *
+     * @param nums - an array of integers.
+     * @return - the minimum difference between the largest and smallest value of nums after performing at most three
+     * moves. In one move, you can choose one element of nums and change it to any value.
+     */
+    public int minDifference(int[] nums) {
+        if (nums.length <= 4) return 0;
+
+        PriorityQueue<Integer> highest = new PriorityQueue<>();
+        PriorityQueue<Integer> lowest = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int number : nums) {
+            highest.offer(number);
+            lowest.offer(number);
+            if (highest.size() > 4) highest.poll();
+            if (lowest.size() > 4) lowest.poll();
+        }
+
+        int minDiff = Integer.MAX_VALUE;
+
+        int[] lowestNums = new int[4];
+        for (int i = 0; i < 4; i++) {
+            lowestNums[i] = lowest.poll();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int curDiff = highest.poll() - lowestNums[3 - i];
+            minDiff = Math.min(minDiff, curDiff);
+        }
+
+        return minDiff;
     }
 }
