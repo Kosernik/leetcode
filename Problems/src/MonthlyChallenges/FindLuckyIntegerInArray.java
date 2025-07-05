f93d3b5af3181f3fc0692cf16c25ecd52aada322
Kosernik <kornilovsergei28@gmail.com>
2025-07-05 10:54:09 +0300

    LeetCode №1394.

---
+++ Problems/src/MonthlyChallenges/FindLuckyIntegerInArray.java
@@ -0,0 +1,32 @@
+package MonthlyChallenges;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindLuckyIntegerInArray {
+
+    /**
+     * LeetCode №1394. Find Lucky Integer in an Array.
+     * <p>
+     * Complexity - O(N)
+     * Memory - O(N)
+     *
+     * @param arr - an array of integers.
+     * @return - the largest lucky number or -1 if no lucky number exist.
+     */
+    public int findLucky(int[] arr) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int number : arr) {
+            counts.put(number, counts.getOrDefault(number, 0) + 1);
+        }
+
+        int result = -1;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getKey().intValue() == entry.getValue().intValue()) {
+                result = Math.max(result, entry.getKey());
+            }
+        }
+
+        return result;
+    }
+}
