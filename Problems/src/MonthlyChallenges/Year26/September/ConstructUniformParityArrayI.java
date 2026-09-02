7e2733278f6163d2dfba03f901d14d71fd3bc828
Kosernik <kornilovsergei28@gmail.com>
2026-09-02 09:36:00 +0300

    LeetCode №3875.

---
+++ Problems/src/MonthlyChallenges/Year26/September/ConstructUniformParityArrayI.java
@@ -0,0 +1,19 @@
+package MonthlyChallenges.Year26.September;
+
+public class ConstructUniformParityArrayI {
+
+    /**
+     * LeetCode №3875. Construct Uniform Parity Array I.
+     * <p>
+     * For each index i, you must choose exactly one of the following (in any order):
+     * * nums2[i] = nums1[i]
+     * * nums2[i] = nums1[i] - nums1[j], for an index j != i
+     *
+     * @param nums1 - an array of positive integers.
+     * @return - true if it is possible to construct an array consisting of either all odd or all even numbers.
+     * False - otherwise.
+     */
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
